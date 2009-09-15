312506ee1c289077c4a06e14924464984463f979
Anders Pearson <anders@columbia.edu>
2009-09-15 14:07:01 -0400

    moved reorder_section_children to pagetree

--- pagetree/urls.py
+++ pagetree/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('pagetree.views',
                        (r'^reorder_pageblocks/$', 'reorder_pageblocks'),
-                       
+                       (r'^reorder_section_children/(?P<id>\d+)/','reorder_section_children'),
 )
--- pagetree/views.py
+++ pagetree/views.py
@@ -12,3 +12,13 @@ def reorder_pageblocks(request,id):
     pageblocks = [int(request.GET[k]) for k in keys if k.startswith('pageblock_id_')]
     section.update_pageblocks_order(pageblocks)
     return HttpResponse("ok")
+
+def reorder_section_children(request,id):
+    if request.method != "POST":
+        return HttpResponse("only use POST for this")
+    section = get_object_or_404(Section,id=id)
+    keys = request.GET.keys()
+    keys.sort()
+    children = [int(request.GET[k]) for k in keys if k.startswith('section_id_')]
+    section.update_children_order(children)
+    return HttpResponse("ok")
