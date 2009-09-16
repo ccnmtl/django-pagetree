41297655ed814886c540c6bea9b0425a62d65802
Anders Pearson <anders@columbia.edu>
2009-09-16 11:33:29 -0400

    better sorting

--- pagetree/urls.py
+++ pagetree/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('pagetree.views',
                        (r'^reorder_section_children/(?P<id>\d+)/','reorder_section_children'),
                        (r'^delete_pageblock/(?P<id>\d+)/$','delete_pageblock'),
                        (r'^edit_pageblock/(?P<id>\d+)/$','edit_pageblock'),
-
+                       (r'^delete_section/(?P<id>\d+)/$','delete_section'),
+                       (r'^edit_section/(?P<id>\d+)/$','edit_section'),
 )
--- pagetree/views.py
+++ pagetree/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from pagetree.models import Section, PageBlock
+from django.template.defaultfilters import slugify
 
 def reorder_pageblocks(request,id):
     if request.method != "POST":
         return HttpResponse("only use POST for this")
     section = get_object_or_404(Section,id=id)
     keys = request.GET.keys()
-    # TODO: numerical sort order bug in these with, eg, pageblock_id_9, pageblock_id_10
-    keys.sort()
+    keys.sort(key=lambda x: int(x.split('_')[-1]))
     pageblocks = [int(request.GET[k]) for k in keys if k.startswith('pageblock_id_')]
     section.update_pageblocks_order(pageblocks)
     return HttpResponse("ok")
@@ -18,7 +18,7 @@ def reorder_section_children(request,id):
         return HttpResponse("only use POST for this")
     section = get_object_or_404(Section,id=id)
     keys = request.GET.keys()
-    keys.sort()
+    keys.sort(key=lambda x: int(x.split('_')[-1]))
     children = [int(request.GET[k]) for k in keys if k.startswith('section_id_')]
     section.update_children_order(children)
     return HttpResponse("ok")
@@ -36,3 +36,21 @@ def edit_pageblock(request,id):
     section = block.section
     block.edit(request.POST,request.FILES)
     return HttpResponseRedirect("/edit" + section.get_absolute_url())
+
+def edit_section(request,id):
+    section = get_object_or_404(Section,id=id)
+    section.label = request.POST.get('label','')
+    section.slug = request.POST.get('slug',slugify(section.label))
+    section.save()
+    return HttpResponseRedirect("/edit" + section.get_absolute_url())
+
+def delete_section(request,id):
+    section = get_object_or_404(Section,id=id)
+    if request.method == "POST":
+        parent = section.get_parent()
+        section.delete()
+        return HttpResponseRedirect("/edit" + parent.get_absolute_url())
+    return HttpResponse("""
+<html><body><form action="." method="post">Are you Sure?
+<input type="submit" value="Yes, delete it" /></form></body></html>
+""")
