5389b03dcd0a550f08e46b6a9b55a4736f8d28ff
Anders Pearson <anders@columbia.edu>
2009-10-01 16:18:22 -0400

    switched to named urls to make some things a little cleaner

--- pagetree/urls.py
+++ pagetree/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('pagetree.views',
-                       (r'^reorder_pageblocks/$', 'reorder_pageblocks'),
-                       (r'^reorder_section_children/(?P<id>\d+)/','reorder_section_children'),
-                       (r'^delete_pageblock/(?P<id>\d+)/$','delete_pageblock'),
-                       (r'^edit_pageblock/(?P<id>\d+)/$','edit_pageblock'),
-                       (r'^delete_section/(?P<id>\d+)/$','delete_section'),
-                       (r'^edit_section/(?P<id>\d+)/$','edit_section'),
-                       (r'^section/(?P<id>\d+)/add_pageblock/$','add_pageblock'),
-                       (r'^section/(?P<id>\d+)/add_child_section/$','add_child_section'),
+                       (r'^reorder_pageblocks/$', 'reorder_pageblocks',{},"reorder-pageblocks"),
+                       (r'^reorder_section_children/(?P<id>\d+)/','reorder_section_children',{},"reorder-section-children"),
+                       (r'^delete_pageblock/(?P<id>\d+)/$','delete_pageblock',{},"delete-pageblock"),
+                       (r'^edit_pageblock/(?P<id>\d+)/$','edit_pageblock',{},"edit-pageblock"),
+                       (r'^delete_section/(?P<id>\d+)/$','delete_section',{},"delete-section"),
+                       (r'^edit_section/(?P<id>\d+)/$','edit_section',{},"edit-section"),
+                       (r'^section/(?P<id>\d+)/add_pageblock/$','add_pageblock',{},"add-pageblock"),
+                       (r'^section/(?P<id>\d+)/add_child_section/$','add_child_section',{},"add-child-section"),
 )
