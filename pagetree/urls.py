50d06f52a6f2c2b6b30d9f216a5be7d5570df2bd
sdreher <sdreher@gmail.com>
2009-12-04 18:54:01 -0500

    * Changing the add dialog to be a select box with shown/hidden forms as the selection changes. Replaces a long list of forms.

--- pagetree/urls.py
+++ pagetree/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls.defaults import patterns
+import os.path
+media_root = os.path.join(os.path.dirname(__file__),"media")
 
-urlpatterns = patterns('pagetree.views',
+urlpatterns = patterns('',
+                       (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': media_root}),
+)
+
+urlpatterns += patterns('pagetree.views',
                        (r'^reorder_pageblocks/(?P<id>\d+)/', 'reorder_pageblocks',{},"reorder-pageblocks"),
                        (r'^reorder_section_children/(?P<id>\d+)/','reorder_section_children',{},"reorder-section-children"),
                        (r'^section/add/(?P<section_id>\d+)/$', 'add_childsection', {}, "add-childsection"),
