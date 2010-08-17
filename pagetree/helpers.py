ebbe2967b8da65e6dca0616017b24da115103c04
Anders Pearson <anders@columbia.edu>
2010-08-17 15:50:11 -0400

    added submitted() and block_submitted() to helpers

--- pagetree/helpers.py
+++ pagetree/helpers.py
@@ -21,3 +21,35 @@ def needs_submit(section):
             if p.block().needs_submit():
                 return True
     return False
+
+def submitted(section,user):
+    """ if all blocks on the page that require submissions
+    have been submitted """
+    for p in section.pageblock_set.all():
+        if hasattr(p.block(),'needs_submit'):
+            if p.block().needs_submit():
+                try:
+                    s = p.block().unlocked(user)
+                    if not s:
+                        # there's an unsubmitted block
+                        return False
+                except:
+                    # most likely: no unlocked() method
+                    pass
+    # made it all the way through without any blocks
+    # reporting that they are unsubmitted
+    return True
+
+def block_submitted(block,user):
+    if user.is_anonymous():
+        # anon can't have submitted a block
+        return False
+    if hasattr(block,'needs_submit'):
+        if block.needs_submit():
+            try:
+                s = block.unlocked(user)
+                if not s:
+                    return False
+            except:
+                pass
+    return True
