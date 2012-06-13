85b51abf914f391a663219193fccfb1b5c0882d6
Anders Pearson <anders@columbia.edu>
2012-06-13 11:00:28 -0400

    flake8 compliance

--- pagetree/helpers.py
+++ pagetree/helpers.py
@@ -1,12 +1,15 @@
 from models import Hierarchy
 
+
 def get_hierarchy(name="main"):
     return Hierarchy.objects.get_or_create(name=name,
                                            defaults=dict(base_url="/"))[0]
 
-def get_section_from_path(path,hierarchy="main"):
+
+def get_section_from_path(path, hierarchy="main"):
     h = get_hierarchy(hierarchy)
     return h.get_section_from_path(path)
+
 
 def get_module(section):
     """ get the top level module that the section is in"""
@@ -16,19 +19,21 @@ def get_module(section):
         return section
     return section.get_ancestors()[1]
 
+
 def needs_submit(section):
     """ if any blocks on the page need to be submitted """
     for p in section.pageblock_set.all():
-        if hasattr(p.block(),'needs_submit'):
+        if hasattr(p.block(), 'needs_submit'):
             if p.block().needs_submit():
                 return True
     return False
 
-def submitted(section,user):
+
+def submitted(section, user):
     """ if all blocks on the page that require submissions
     have been submitted """
     for p in section.pageblock_set.all():
-        if hasattr(p.block(),'needs_submit'):
+        if hasattr(p.block(), 'needs_submit'):
             if p.block().needs_submit():
                 try:
                     s = p.block().unlocked(user)
@@ -42,11 +47,12 @@ def submitted(section,user):
     # reporting that they are unsubmitted
     return True
 
-def block_submitted(block,user):
+
+def block_submitted(block, user):
     if user.is_anonymous():
         # anon can't have submitted a block
         return False
-    if hasattr(block,'needs_submit'):
+    if hasattr(block, 'needs_submit'):
         if block.needs_submit():
             try:
                 s = block.unlocked(user)
--- pagetree/models.py
+++ pagetree/models.py
@@ -6,27 +6,27 @@ from django.http import Http404
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.db.models import get_model
-from django.core.cache import cache
 from django.template.defaultfilters import slugify
 from treebeard.mp_tree import MP_Node
 
 settings = None
 try:
     from django.conf import settings
-except:
+except ImportError:
     # if we can't import settings, it just means
     # they won't be able to get a list of available
     # pageblock classes
-    settings = None
- 
+    pass
+
 
 class Hierarchy(models.Model):
     name = models.CharField(max_length=256)
-    base_url = models.CharField(max_length=256,default="")
+    base_url = models.CharField(max_length=256, default="")
 
     @staticmethod
     def get_hierarchy(name):
-        return Hierarchy.objects.get_or_create(name=name,defaults=dict(base_url="/"))[0]
+        return Hierarchy.objects.get_or_create(name=name,
+                                               defaults=dict(base_url="/"))[0]
 
     def get_absolute_url(self):
         return self.base_url
@@ -37,14 +37,15 @@ class Hierarchy(models.Model):
     def get_root(self):
         # will create it if it doesn't exist
         try:
-            return Section.objects.get(hierarchy=self,label="Root").get_root()
+            return Section.objects.get(hierarchy=self,
+                                       label="Root").get_root()
         except Section.DoesNotExist:
-            return Section.add_root(label="Root",slug="",hierarchy=self)
+            return Section.add_root(label="Root", slug="", hierarchy=self)
 
     def get_top_level(self):
         return self.get_root().get_children()
 
-    def get_section_from_path(self,path):
+    def get_section_from_path(self, path):
         if path.endswith("/"):
             path = path[:-1]
         root = self.get_root()
@@ -55,29 +56,28 @@ class Hierarchy(models.Model):
             slugs = dict()
             for c in current.get_children():
                 slugs[c.slug] = c
-            if slugs.has_key(slug):
+            if slug in slugs:
                 current = slugs[slug]
             else:
                 raise Http404()
         return current
 
     def available_pageblocks(self):
-        if hasattr(settings,'PAGEBLOCKS'):
+        if hasattr(settings, 'PAGEBLOCKS'):
             return [get_model(*pb.split('.')) for pb in settings.PAGEBLOCKS]
         else:
             return []
 
-    def get_first_leaf(self,section):
+    def get_first_leaf(self, section):
         if (section.is_leaf()):
             return section
-    
         return self.get_first_leaf(section.get_children()[0])
 
-    def get_last_leaf(self,section):
+    def get_last_leaf(self, section):
         if (section.is_leaf()):
             return section
-    
         return self.get_last_leaf(section.get_children()[-1])
+
 
 class Section(MP_Node):
     label = models.CharField(max_length=256)
@@ -101,7 +101,7 @@ class Section(MP_Node):
     def closing_children(self):
         """ this returns the list of adjacent last children.
         we need this to know how many levels deep need to be closed
-        when making the menus as flattened nested lists. 
+        when making the menus as flattened nested lists.
         look for 'closing_children' in a menu.html in a project
         that uses pagetree to see exactly what i mean. """
 
@@ -113,11 +113,11 @@ class Section(MP_Node):
     def get_previous(self):
         # previous node in the depth-first traversal
         depth_first_traversal = self.get_root().get_annotated_list()
-        for (i,(s,ai)) in enumerate(depth_first_traversal):
+        for (i, (s, ai)) in enumerate(depth_first_traversal):
             if s.id == self.id:
                 # make sure we don't return the root
-                if i > 1 and not depth_first_traversal[i-1][0].is_root():
-                    return depth_first_traversal[i-1][0]
+                if i > 1 and not depth_first_traversal[i - 1][0].is_root():
+                    return depth_first_traversal[i - 1][0]
                 else:
                     return None
         # made it through without finding ourselves? weird.
@@ -126,23 +126,25 @@ class Section(MP_Node):
     def get_next(self):
         # next node in the depth-first traversal
         depth_first_traversal = self.get_root().get_annotated_list()
-        for (i,(s,ai)) in enumerate(depth_first_traversal):
+        for (i, (s, ai)) in enumerate(depth_first_traversal):
             if s.id == self.id:
                 if i < len(depth_first_traversal) - 1:
-                    return depth_first_traversal[i+1][0]
+                    return depth_first_traversal[i + 1][0]
                 else:
                     return None
         # made it through without finding ourselves? weird.
         return None
 
-    def append_child(self,label,slug=''):
+    def append_child(self, label, slug=''):
         if slug == '':
             slug = slugify(label)
-        return self.add_child(label=label,slug=slug,hierarchy=self.hierarchy)
+        return self.add_child(label=label, slug=slug, hierarchy=self.hierarchy)
 
-    def append_pageblock(self,label,content_object):
+    def append_pageblock(self, label, content_object):
         neword = self.pageblock_set.count() + 1
-        return PageBlock.objects.create(section=self,label=label,ordinality=neword,content_object=content_object)
+        return PageBlock.objects.create(section=self, label=label,
+                                        ordinality=neword,
+                                        content_object=content_object)
 
     def __unicode__(self):
         return self.label
@@ -175,7 +177,7 @@ class Section(MP_Node):
             slug = forms.CharField(initial=self.slug)
         return EditSectionForm()
 
-    def update_children_order(self,children_ids):
+    def update_children_order(self, children_ids):
         """children_ids is a list of Section ids for the children
         in the order that they should be set to.
 
@@ -185,51 +187,51 @@ class Section(MP_Node):
         for section_id in children_ids:
             s = Section.objects.get(id=section_id)
             p = s.get_parent()
-            s.move(p,pos="last-child")
-        return 
+            s.move(p, pos="last-child")
+        return
 
-    def update_pageblocks_order(self,pageblock_ids):
+    def update_pageblocks_order(self, pageblock_ids):
         """pageblock_ids is a list of PageBlock ids for the children
         in the order that they should be set to.
 
         use with caution. if the ids in pageblock_ids don't match up
         right it will break or do strange things.
         """
-        for (i,id) in enumerate(pageblock_ids):
+        for (i, id) in enumerate(pageblock_ids):
             sc = PageBlock.objects.get(id=id)
             sc.ordinality = i + 1
             sc.save()
 
     def available_pageblocks(self):
         return self.hierarchy.available_pageblocks()
+
     def add_pageblock_form(self):
         class EditForm(forms.Form):
             label = forms.CharField()
         return EditForm()
+
     def get_first_leaf(self):
         if (self.is_leaf()):
             return self
-    
         return self.get_children()[0].get_first_leaf()
 
     def get_last_leaf(self):
         if (self.is_leaf()):
             return self
-    
         return self.get_children()[-1].get_last_leaf()
 
-    def reset(self,user):
+    def reset(self, user):
         """ clear a user's responses to all pageblocks on this page """
         for p in self.pageblock_set.all():
-            if hasattr(p.block(),'needs_submit'):
+            if hasattr(p.block(), 'needs_submit'):
                 if p.block().needs_submit():
                     p.block().clear_user_submissions(user)
 
-    def submit(self,request_data,user):
+    def submit(self, request_data, user):
         """ store users's responses to the pageblocks on this page """
         proceed = True
         for p in self.pageblock_set.all():
-            if hasattr(p.block(),'needs_submit'):
+            if hasattr(p.block(), 'needs_submit'):
                 if p.block().needs_submit():
                     prefix = "pageblock-%d-" % p.id
                     data = dict()
@@ -241,13 +243,14 @@ class Section(MP_Node):
                                 data[k[len(prefix):]] = request_data[k]
                             else:
                                 data[k[len(prefix):]] = v
-                    p.block().submit(user,data)
-                    if hasattr(p.block(),'redirect_to_self_on_submit'):
+                    p.block().submit(user, data)
+                    if hasattr(p.block(), 'redirect_to_self_on_submit'):
                         # semi bug here?
                         # proceed will only be set by the last submittable
                         # block on the page. previous ones get ignored.
                         proceed = not p.block().redirect_to_self_on_submit()
         return proceed
+
 
 class PageBlock(models.Model):
     section = models.ForeignKey(Section)
@@ -259,24 +262,26 @@ class PageBlock(models.Model):
     content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     class Meta:
-        ordering = ('section','ordinality',)
+        ordering = ('section', 'ordinality',)
 
     def __unicode__(self):
-        return "%s [%d]: %s" % (self.section.label,self.ordinality,self.label)
+        return "%s [%d]: %s" % (self.section.label, self.ordinality,
+                                self.label)
 
     def block(self):
         return self.content_object
 
     def edit_label(self):
-        """ provide a label for the pageblock to make the edit interface easier to read """
-        if hasattr(self.block(),'edit_label'):
+        """ provide a label for the pageblock to make the
+        edit interface easier to read """
+        if hasattr(self.block(), 'edit_label'):
             return self.block().edit_label()
         else:
             return self.block().display_name
 
-    def render(self,**kwargs):
-        if hasattr(self.content_object,"template_file"):
-            t = get_template(getattr(self.content_object,"template_file"))
+    def render(self, **kwargs):
+        if hasattr(self.content_object, "template_file"):
+            t = get_template(getattr(self.content_object, "template_file"))
             d = kwargs
             d['block'] = self.content_object
             c = Context(d)
@@ -284,30 +289,29 @@ class PageBlock(models.Model):
         else:
             return self.content_object.render()
 
-    def render_js(self,**kwargs):
-        if hasattr(self.content_object,"js_template_file"):
-            t = get_template(getattr(self.content_object,"js_template_file"))
+    def render_js(self, **kwargs):
+        if hasattr(self.content_object, "js_template_file"):
+            t = get_template(getattr(self.content_object, "js_template_file"))
             d = kwargs
             d['block'] = self.content_object
             c = Context(d)
             return t.render(c)
-        elif hasattr(self.content_object,"js_render"):
+        elif hasattr(self.content_object, "js_render"):
             return self.content_object.js_render()
         else:
             return ""
 
-    def render_css(self,**kwargs):
-        if hasattr(self.content_object,"css_template_file"):
-            t = get_template(getattr(self.content_object,"css_template_file"))
+    def render_css(self, **kwargs):
+        if hasattr(self.content_object, "css_template_file"):
+            t = get_template(getattr(self.content_object, "css_template_file"))
             d = kwargs
             d['block'] = self.content_object
             c = Context(d)
             return t.render(c)
-        elif hasattr(self.content_object,"css_render"):
+        elif hasattr(self.content_object, "css_render"):
             return self.content_object.css_render()
         else:
             return ""
-
 
     def default_edit_form(self):
         class EditForm(forms.Form):
@@ -317,16 +321,16 @@ class PageBlock(models.Model):
     def edit_form(self):
         return self.content_object.edit_form()
 
-    def edit(self,vals,files):
-        self.label = vals.get('label','')
+    def edit(self, vals, files):
+        self.label = vals.get('label', '')
         self.save()
-        self.content_object.edit(vals,files)
-        
+        self.content_object.edit(vals, files)
+
     def delete(self):
-         section = self.section
-         super(PageBlock, self).delete() # Call the "real" delete() method
-         section.renumber_pageblocks()
-        
+        section = self.section
+        super(PageBlock, self).delete()  # Call the "real" delete() method
+        section.renumber_pageblocks()
+
     def as_dict(self):
         d = dict()
         if hasattr(self.content_object, 'as_dict'):
