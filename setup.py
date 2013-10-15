1d40d8759f4925ddc4afb98ca3127a515197a3ca
Anders Pearson <anders@columbia.edu>
2013-10-15 11:59:11 -0400

    django 1.5 compatability

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-pagetree",
-    version="0.12.2",
+    version="0.12.3",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="http://wiki.ccnmtl.columbia.edu/",
