753445f618379f7590f0aadbfc4642523b72d9e0
Susan Dreher <sdreher@columbia.edu>
2014-06-26 11:43:05 -0400

    updating version numbers.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-pagetree",
-    version="0.14.4",
+    version="0.14.5",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="http://wiki.ccnmtl.columbia.edu/",
