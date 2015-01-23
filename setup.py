9ad484e4a06eca04c238ba8eaba733d4f1504968
Nik Nyby <nnyby@columbia.edu>
2015-01-23 13:28:37 -0500

    Version 1.0.4
    Releasing a new version since I'll need it for WORTH.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-pagetree",
-    version="1.0.3",
+    version="1.0.4",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="https://github.com/ccnmtl/django-pagetree",
