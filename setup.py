9c3d52a1443c6371e38d7c58120825185957963a
Susan Dreher <sdreher@columbia.edu>
2015-02-24 09:50:05 -0500

    CSRF protection for the import json form

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-pagetree",
-    version="1.0.6",
+    version="1.0.7",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="https://github.com/ccnmtl/django-pagetree",
