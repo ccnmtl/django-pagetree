cf9c2f5619a76e059108bce5db95a24c28e531af
Anders Pearson <anders@columbia.edu>
2016-08-02 13:26:40 +0100

    flake8 fixes

--- pagetree/tests/urls.py
+++ pagetree/tests/urls.py
@@ -9,29 +9,29 @@ urlpatterns = [
     # instantiate generic views using a template that doesn't
     # extend/depend on base.html
     url(r'^pages/edit/(?P<path>.*)$',
-     EditView.as_view(template_name="pagetree/test_page.html"),
-     {}, 'edit-page'),
+        EditView.as_view(template_name="pagetree/test_page.html"),
+        {}, 'edit-page'),
     url(r'^pages/instructor/(?P<path>.*)$',
-     InstructorView.as_view(template_name="pagetree/test_page.html")),
+        InstructorView.as_view(template_name="pagetree/test_page.html")),
     url(r'^pages/(?P<path>.*)$',
-     PageView.as_view(template_name="pagetree/test_page.html")),
+        PageView.as_view(template_name="pagetree/test_page.html")),
 
     # a second set to make sure non-default hierarchies
     # get handled as well
     url(r'^pages2/edit/(?P<path>.*)$',
-     EditView.as_view(
-         hierarchy_name="two",
-         hierarchy_base="/pages2",
-         template_name="pagetree/test_page.html"),
-     {}, 'edit-page'),
+        EditView.as_view(
+            hierarchy_name="two",
+            hierarchy_base="/pages2",
+            template_name="pagetree/test_page.html"),
+        {}, 'edit-page'),
     url(r'^pages2/instructor/(?P<path>.*)$',
-     InstructorView.as_view(
-         hierarchy_name="two",
-         hierarchy_base="/pages2",
-         template_name="pagetree/test_page.html")),
+        InstructorView.as_view(
+            hierarchy_name="two",
+            hierarchy_base="/pages2",
+            template_name="pagetree/test_page.html")),
     url(r'^pages2/(?P<path>.*)$',
-     PageView.as_view(
-         hierarchy_name="two",
-         hierarchy_base="/pages2",
-         template_name="pagetree/test_page.html")),
+        PageView.as_view(
+            hierarchy_name="two",
+            hierarchy_base="/pages2",
+            template_name="pagetree/test_page.html")),
 ]
