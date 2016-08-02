920eeaeb5b4f483069a6854a4f8ab1ac5aa00228
Anders Pearson <anders@columbia.edu>
2016-08-02 13:07:24 +0100

    modern urls.py style

--- pagetree/tests/urls.py
+++ pagetree/tests/urls.py
@@ -1,38 +1,37 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, include
+from django.conf.urls import include, url
 from pagetree.generic.views import EditView, InstructorView, PageView
 
-urlpatterns = patterns(
-    '',
-    (r'^pagetree/', include('pagetree.urls')),
+urlpatterns = [
+    url(r'^pagetree/', include('pagetree.urls')),
 
     # instantiate generic views using a template that doesn't
     # extend/depend on base.html
-    (r'^pages/edit/(?P<path>.*)$',
+    url(r'^pages/edit/(?P<path>.*)$',
      EditView.as_view(template_name="pagetree/test_page.html"),
      {}, 'edit-page'),
-    (r'^pages/instructor/(?P<path>.*)$',
+    url(r'^pages/instructor/(?P<path>.*)$',
      InstructorView.as_view(template_name="pagetree/test_page.html")),
-    (r'^pages/(?P<path>.*)$',
+    url(r'^pages/(?P<path>.*)$',
      PageView.as_view(template_name="pagetree/test_page.html")),
 
     # a second set to make sure non-default hierarchies
     # get handled as well
-    (r'^pages2/edit/(?P<path>.*)$',
+    url(r'^pages2/edit/(?P<path>.*)$',
      EditView.as_view(
          hierarchy_name="two",
          hierarchy_base="/pages2",
          template_name="pagetree/test_page.html"),
      {}, 'edit-page'),
-    (r'^pages2/instructor/(?P<path>.*)$',
+    url(r'^pages2/instructor/(?P<path>.*)$',
      InstructorView.as_view(
          hierarchy_name="two",
          hierarchy_base="/pages2",
          template_name="pagetree/test_page.html")),
-    (r'^pages2/(?P<path>.*)$',
+    url(r'^pages2/(?P<path>.*)$',
      PageView.as_view(
          hierarchy_name="two",
          hierarchy_base="/pages2",
          template_name="pagetree/test_page.html")),
-)
+]
