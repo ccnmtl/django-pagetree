08c3e2537350b7c2d54560aa95b9073b16ad6377
Anders Pearson <anders@columbia.edu>
2017-01-18 13:19:42 +0000

    :bookmark: release 1.2.5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-pagetree",
-    version="1.2.4",
+    version="1.2.5",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="https://github.com/ccnmtl/django-pagetree",
