15afe76dff2c0db4164df540fd00ff6c2d2e30c2
Nikolas Nyby <nnyby@columbia.edu>
2026-03-25 11:02:15 -0400

    :bookmark: release version 1.6.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-pagetree",
-    version="1.6.2",
+    version="1.6.3",
     author="Anders Pearson",
     author_email="ctl-dev@columbia.edu",
     url="https://github.com/ccnmtl/django-pagetree",
